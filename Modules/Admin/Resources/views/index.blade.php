a797fd4aa0f72bfeef35583ab1e9619599d5047d
ducpv2 <ducpv2@vnext.com.vn>
2018-12-07 16:01:29 +0700

    css

--- Modules/Admin/Resources/views/index.blade.php
+++ Modules/Admin/Resources/views/index.blade.php
@@ -13,12 +13,10 @@
                                         <div class="logo"><img src="https://colorlib.com/polygon/adminator/assets/static/images/logo.png" alt=""></div>
                                     </div>
                                     <div class="peer peer-greed">
-                                        <h5 class="lh-1 mB-0 logo-text">Adminator</h5></div>
+                                        <h5 class="lh-1 mB-0 logo-text">Adminator</h5>
+                                    </div>
                                 </div>
                             </a>
-                        </div>
-                        <div class="peer">
-                            <div class="mobile-toggle sidebar-toggle"><a href="" class="td-n"><i class="ti-arrow-circle-left"></i></a></div>
                         </div>
                     </div>
                 </div>
@@ -26,19 +24,19 @@
                 <ul class="sidebar-menu scrollable pos-r ps ps--active-y">
                     <li class="nav-item mT-30 active">
                         <a class="sidebar-link" href="https://colorlib.com/polygon/adminator/index.html" default="">
-                            <span class="icon-holder"><i class="fa fa-home" aria-hidden="true"></i> </span>
+                            <span class="icon-holder"><i class="c-blue-500 ti-home"></i> </span>
                             <span class="title">Dashboard</span>
                         </a>
                     </li>
                     <li class="nav-item dropdown">
                         <a class="sidebar-link" href="ui.html">
-                            <span class="icon-holder"><i class="fa fa-home" aria-hidden="true"></i> </span>
+                            <span class="icon-holder"><i class="c-brown-500 ti-email"></i> </span>
                             <span class="title">UI Elements</span>
                         </a>
                     </li>
-                    <li class="nav-item dropdown">
+                    <li class="nav-item dropdown open">
                         <a class="dropdown-toggle" href="javascript:void(0);">
-                            <span class="icon-holder"><i class="fa fa-home" aria-hidden="true"></i> </span>
+                            <span class="icon-holder"><i class="c-indigo-500 ti-bar-chart"></i> </span>
                             <span class="title">Tables</span> <span class="arrow"><i class="ti-angle-right"></i></span>
                         </a>
                         <ul class="dropdown-menu">
@@ -51,7 +49,21 @@
             </div>
         </div>
         <div class="main-conten bg-w mh-100vh">
-
+            <div class="header navbar">
+                <div class="header-container">
+                        <ul class="nav-left">
+                            <li><a id="sidebar-toggle" class="sidebar-toggle" href="javascript:void(0);"><i class="ti-menu" aria-hidden="true"></i></a></li>
+                            <li class="search-box active">
+                                <a class="search-toggle no-pdd-right" href="javascript:void(0);">
+                                    <i class="search-icon ti-search pdd-right-10" aria-hidden="true"></i>
+                                    <i class="search-icon-close ti-close pdd-right-10"></i>
+                                </a></li>
+                            <li class="search-input active">
+                                <input class="form-control" type="text" placeholder="Search...">
+                            </li>
+                        </ul>
+                </div>
+            </div>     
         </div>
     </div>
 @stop
