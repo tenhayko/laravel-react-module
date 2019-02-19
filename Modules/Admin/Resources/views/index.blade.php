92a87cf8475a3a0e25e9a111f4a1f96606499a11
ducpv2 <ducpv2@vnext.com.vn>
2019-02-19 10:35:38 +0700

    remove footer

--- Modules/Admin/Resources/views/index.blade.php
+++ Modules/Admin/Resources/views/index.blade.php
@@ -199,9 +199,9 @@
                 </div>
             </main>
             {{-- end main --}}
-            <footer class="bdT ta-c p-30 lh-0 fsz-sm c-grey-600">
+            {{-- <footer class="bdT ta-c p-30 lh-0 fsz-sm c-grey-600">
                 <span>Copyright © 2017 Designed by <a href="https://colorlib.com" target="_blank" title="Colorlib">Colorlib</a>. All rights reserved.</span>
-            </footer>
+            </footer> --}}
         </div>
     </div>
 @stop
